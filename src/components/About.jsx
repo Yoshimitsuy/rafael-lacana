7da20ba11c3e3832a77a61f6cb79a77f6653f4f7
yoshimitsuy <rafael.lacana7@gmail.com>
2023-02-13 02:55:02 -0300

    nuclear commit xD

---
+++ src/components/About.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name='about' className='w-full h-screen bg-[#0a192f] text-gray-300' >
+
+      <div className='flex flex-col justify-center items-center w-full h-full'>
+
+        <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
+
+          <div className='sm:text-right pb-8 pl-4'>
+            <p className='text-4xl font-bold inline border-b-4 border-pink-600'>About</p>
+          </div>
+
+          <div></div>
+
+        </div>
+
+        <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
+          <div className='sm:text-right text-4xl font-bold'>
+            <p>Hi, I'm Clint, nice to meet you. Please take a look around</p>
+          </div>
+          <div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          Proin eros massa, convallis eu sem a, maximus auctor diam. 
+          Ut vitae dolor urna. Praesent quis malesuada erat, id feugiat ligula. 
+          Nullam sed augue velit. Integer pharetra a arcu ut bibendum. 
+          Vivamus consectetur quam at enim pretium, bibendum sodales nibh lacinia. 
+          Etiam sodales gravida sapien eu elementum.</p>
+          </div>
+        </div>
+
+
+
+      </div>
+
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/Home.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { HiArrowNarrowRight } from 'react-icons/hi';
+
+const Home = () => {
+  return (
+    <div name='home' className='w-full h-screen bg-[#0a192f]'>
+      
+      {/* container */}
+      <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+        
+        <p className='text-pink-600'>Hi, my name is</p>
+        
+        <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Rafael Yoshimitsuy</h1>
+        
+        <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Full Stack Developer</h2>
+
+        <p className='text-[#8892b0] py-4 max-w-[700px]'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          Proin eros massa, convallis eu sem a, maximus auctor diam. 
+          Ut vitae dolor urna. Praesent quis malesuada erat, id feugiat ligula. 
+          Nullam sed augue velit. Integer pharetra a arcu ut bibendum. 
+          Vivamus consectetur quam at enim pretium, bibendum sodales nibh lacinia. 
+          Etiam sodales gravida sapien eu elementum.
+        </p>
+
+        <div>
+          <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
+            View Work 
+            <span className='group-hover:rotate-90 duration-300'>
+              <HiArrowNarrowRight className='ml-3'/>
+            </span>
+            </button>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Home
