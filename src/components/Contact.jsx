af7d69a6382c91e0738d97614d1deb07d6d1f2c2
yoshimitsuy <rafael.lacana7@gmail.com>
2023-02-14 00:40:56 -0300

    form config

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,8 +3,16 @@ import React from 'react'
 const Contact = () => {
   return (
     <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
-      <form action=''>
-        1:21:15:44:23:204:1394
+      <form method='POST' action='https://getform.io/f/5a06de45-0af7-4c3c-91c1-7352bd246904' className='flex flex-col max-w-[600px] w-full'>
+        <div className='pb-8'>
+          <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
+          <p className='text-gray-300 py-4'>\\ Submit the form below or shoot me an email - rafael.lacana@hotmail.com</p>
+        </div>
+
+        <input className='bg-[#ccd6f6] p-2' type='text' placeholder='name' name='name' />
+        <input className='my-4 p-2 bg-[#ccd6f6]' type='email' placeholder='email' name='email' />
+        <textarea className='bg-[#ccd6f6] p-2' name='message' rows='10' placeholder='your message'></textarea>
+        <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center'>bruxaria</button>
       </form>
     </div>
   )
