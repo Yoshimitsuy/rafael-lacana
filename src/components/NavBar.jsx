9500470225facfa4fd472313084e127954e995bd
yoshimitsuy <rafael.lacana7@gmail.com>
2023-02-14 02:51:44 -0300

    links and socials

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { FaBars, FaTimes, FaGithub, FaLinkedin, FaMailBulk } from 'react-icons/fa'
+import { FaBars, FaTimes, FaGithub, FaLinkedin, FaMailBulk, FaInstagram, FaTwitter, FaTwitch, FaSpotify } from 'react-icons/fa'
 // import { HiOutlineMail } from 'react-icons/hi';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
 import Logo from '../assets/yoshimitsuy-logo.png'
@@ -84,30 +84,54 @@ const NavBar = () => {
         </ul>
 
       {/* Social Icons */}
-      <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+      <div className='hidden lg:flex fixed flex-col top-[25%] left-0'>
         <ul>
           
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
-            <a className='flex justify-between items-center w-full text-gray-300' href='/'>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#0077b5]'>
+            <a className='flex justify-between items-center w-full font-bold text-gray-300' href='https://www.linkedin.com/in/rafael-lacana/' target='_blank' rel='noreferrer'>
               LinkedIn <FaLinkedin size={30} />
             </a>
           </li>
 
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
-            <a className='flex justify-between items-center w-full text-gray-300' href='/'>
+            <a className='flex justify-between items-center w-full font-bold text-gray-300' href='https://github.com/Yoshimitsuy' target='_blank' rel='noreferrer'>
               GitHub <FaGithub size={30} />
             </a>
           </li>
 
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#48b9a1]'>
-            <a className='flex justify-between items-center w-full text-gray-300' href='/'>
-              Email <FaMailBulk size={30} />
+            <a className='flex justify-between items-center w-full font-bold text-gray-800' href='/' target='_blank' rel='noreferrer'>
+              Email<FaMailBulk size={30} />
             </a>
           </li>
 
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
-            <a className='flex justify-between items-center w-full text-gray-300' href='/'>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
+            <a className='flex justify-between items-center w-full font-bold text-gray-300' href='/' target='_blank' rel='noreferrer'>
               Contact <BsFillPersonLinesFill size={30} />
+            </a>
+          </li>
+
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#E4405F]'>
+            <a className='flex justify-between items-center w-full font-bold text-gray-300' href='https://www.instagram.com/yoshimitsuy/' target='_blank' rel='noreferrer'>
+              Instagram <FaInstagram size={30} />
+            </a>
+          </li>
+
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#1DA1F2]'>
+            <a className='flex justify-between items-center w-full font-bold text-gray-300' href='https://twitter.com/Yoshimitsuy' target='_blank' rel='noreferrer'>
+              Twitter <FaTwitter size={30} />
+            </a>
+          </li>
+
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#9146FF]'>
+            <a className='flex justify-between items-center w-full font-bold text-gray-800' href='https://www.twitch.tv/yoshimitsuy_' target='_blank' rel='noreferrer'>
+              Twitch <FaTwitch size={30} />
+            </a>
+          </li>
+
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#1ED760]'>
+            <a className='flex justify-between items-center w-full font-bold text-gray-800' href='/' target='_blank' rel='noreferrer'>
+              Spotify <FaSpotify size={30} />
             </a>
           </li>
 
